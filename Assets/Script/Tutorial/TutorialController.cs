3d94bafbd4d1a3c0b7062f8a832448b43fcc8117
kousukei <famas550@gmail.com>
2023-10-19 00:29:16 +0900

    平面鏡も説明まで

--- Assets/Script/Tutorial/TutorialController.cs
+++ Assets/Script/Tutorial/TutorialController.cs
@@ -8,6 +8,11 @@ public class TutorialController : MonoBehaviour
     int progress=0;
     GameObject circle, arrow, explanation, letter;
     Text letterText;
+    bool gameTime;
+    public float a;
+    public float b;
+    public float c;
+    public float d;
     void Start()
     {
         circle = this.transform.Find("Circle").gameObject;
@@ -17,32 +22,19 @@ public class TutorialController : MonoBehaviour
         letterText = letter.GetComponent<Text>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        position(progress);
         if (Input.GetMouseButtonDown(0))
         {
             progress++;
         }
+        g(progress);
     }
     void g(int p)
     {
         switch (p)
-        {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-        }
-    }
-    Vector3 position(int progress)
-    {
-        Vector3 vector=new Vector3();
-        switch (progress)
         {
             case 1:
 
@@ -54,6 +46,149 @@ public class TutorialController : MonoBehaviour
             case 4:
                 break;
         }
-        return vector;
+    }
+    void TimeStop(bool time)
+    {
+        if (time)
+        {
+            Time.timeScale = 0;
+            gameTime = false;
+        }
+    }
+    void TimeStart(bool time)
+    {
+        if (!time)
+        {
+            Time.timeScale = 1;
+            gameTime = true;
+        }
+    }
+    void position(int Progress)
+    {
+        switch (Progress)
+        {
+            case 0:
+                circle.transform.localPosition = new Vector3(-34.5f, 684.6f, 0);
+                circle.transform.localScale = new Vector3(1f, 4.7f, 1f);
+                arrow.transform.localPosition = new Vector3(-132f, 706f, 0);
+
+                arrow.transform.localRotation = new Quaternion(0f, 0f, 1f, -2.3f);
+                explanation.transform.localPosition = new Vector3(-244f, 656f, 0f);
+                explanation.transform.localRotation = new Quaternion(0f, 0f, 1f, 0.93f);
+                explanation.transform.localScale = new Vector3(3.5f, 1.2f, 1.2f);
+                letter.transform.localPosition = new Vector3(-244f, 656f, 0f);
+                letter.transform.localRotation = new Quaternion(0f, 0f, 1f, -1.08f);
+                letter.GetComponent<RectTransform>().sizeDelta = new Vector2(308, 100);
+                letterText.text = "HPバー:\n0の時ゲームオーバー";
+                break;
+            case 1:
+                circle.transform.localPosition = new Vector3(-76f, 682f, 0);
+                circle.transform.localScale = new Vector3(1f, 4.7f, 1f);
+                arrow.transform.localPosition = new Vector3(-132f, 706f, 0);
+
+                arrow.transform.localRotation = new Quaternion(0f, 0f, 1f, -2.3f);
+                explanation.transform.localPosition = new Vector3(-244f, 656f, 0f);
+                explanation.transform.localRotation = new Quaternion(0f, 0f, 1f, 0.93f);
+                explanation.transform.localScale = new Vector3(3.5f, 1.2f, 1.2f);
+                letter.transform.localPosition = new Vector3(-244f, 656f, 0f);
+                letter.transform.localRotation = new Quaternion(0f, 0f, 1f, -1.08f);
+                letter.GetComponent<RectTransform>().sizeDelta = new Vector2(308, 100);
+                letterText.text = "エネルギーバー:\n0の時スキルが使えなくなります";
+                break;
+            case 2:
+                circle.transform.localPosition = new Vector3(-40f, 286f, 0);
+                circle.transform.localScale = new Vector3(1f, 1f, 1f);
+                arrow.transform.localPosition = new Vector3(-134f, 325f, 0);
+
+                arrow.transform.localRotation = new Quaternion(0f, 0f, 1f, -2.3f);
+                explanation.transform.localPosition = new Vector3(-247f, 278f, 0f);
+                explanation.transform.localScale = new Vector3(3.5f, 1.2f, 1.2f);
+                letter.transform.localPosition = new Vector3(-244f, 280f, 0f);
+                letter.transform.localRotation = new Quaternion(0f, 0f, 1f, -1.08f);
+                letter.GetComponent<RectTransform>().sizeDelta = new Vector2(308, 100);
+                letterText.text = "スコア:\n今の点数";
+                break;
+            case 3:
+                circle.transform.localPosition = new Vector3(2,10,0);
+                arrow.transform.localPosition = new Vector3(-86, 0, 0);
+                arrow.transform.localRotation = new Quaternion(0f,0f,1f,-2.3f);
+                explanation.transform.localPosition = new Vector3(-184,-111,0);
+                explanation.transform.localRotation = new Quaternion(0f,0f,1f, 0.93f); 
+                explanation.transform.localScale = new Vector3(3.5f, 1.2f, 1.2f);
+                letter.transform.localPosition = new Vector3(-184, -111, 0);
+                letter.transform.localRotation = new Quaternion(0f, 0f, 1f, -1.08f);
+                letter.GetComponent<RectTransform>().sizeDelta = new Vector2(308, 100);
+                letterText.text = "平面鏡:";
+                break;
+            case 4:
+                circle.transform.localPosition = new Vector3(9.2f, -87f, 0);
+                arrow.transform.localPosition = new Vector3(-86f, -80f, 0);
+
+                arrow.transform.localRotation = new Quaternion(0f, 0f, 1f, -2.3f);
+                explanation.transform.localPosition = new Vector3(-184, -111, 0);
+                explanation.transform.localRotation = new Quaternion(0f, 0f, 1f, 0.93f);
+                explanation.transform.localScale = new Vector3(3.5f, 1.2f, 1.2f);
+                letter.transform.localPosition = new Vector3(-184, -111, 0);
+                letter.transform.localRotation = new Quaternion(0f, 0f, 1f, -1.08f);
+                letter.GetComponent<RectTransform>().sizeDelta = new Vector2(308, 100);
+                letterText.text = "説明0000000";
+                break;
+            case 5:
+                circle.transform.localPosition = new Vector3(18f, -190f, 0f);
+                arrow.transform.localPosition = new Vector3(-72f, -178f, 0f);
+
+                arrow.transform.localRotation = new Quaternion(0f, 0f, 1f, -2.3f);
+                explanation.transform.localPosition = new Vector3(-184f, -111f, 0f);
+                explanation.transform.localRotation = new Quaternion(0f, 0f, 1f, 0.93f);
+                explanation.transform.localScale = new Vector3(3.5f, 1.2f, 1.2f);
+                letter.transform.localPosition = new Vector3(-184f, -111f, 0f);
+                letter.transform.localRotation = new Quaternion(0f, 0f, 1f, -1.08f);
+                letter.GetComponent<RectTransform>().sizeDelta = new Vector2(308, 100);
+                letterText.text = "説明0000000";
+                break;
+            case 6:
+                circle.transform.localPosition = new Vector3(-568.8f, 346.4f, 0f);
+                arrow.transform.localPosition = new Vector3(-469f, 332f, 0f);
+                arrow.transform.localRotation = new Quaternion(0f, 0f, 1f, 0.45f);
+
+                explanation.transform.localPosition = new Vector3(-375f, 259f, 0f);
+                explanation.transform.localRotation = new Quaternion(0f, 0f, 1f, 0.93f);
+                explanation.transform.localScale = new Vector3(3.5f, 1.2f, 1.2f);
+                letter.transform.localPosition = new Vector3(-372f, 261f, 0f);
+                letter.transform.localRotation = new Quaternion(0f, 0f, 1f, -1.08f);
+                letter.GetComponent<RectTransform>().sizeDelta = new Vector2(308f, 100f);
+                letterText.text = "説明0000000";
+                break;
+            case 7:
+                circle.transform.localPosition = new Vector3(-550.9f, 246.3f, 0f);
+                arrow.transform.localPosition = new Vector3(-454.7f, 244.6f, 0f);
+                arrow.transform.localRotation = new Quaternion(0f, 0f, 1f, 0.45f);
+
+                explanation.transform.localPosition = new Vector3(-375f, 259f, 0f);
+                explanation.transform.localRotation = new Quaternion(0f, 0f, 1f, 0.93f);
+                explanation.transform.localScale = new Vector3(3.5f, 1.2f, 1.2f);
+                letter.transform.localPosition = new Vector3(-372f, 261f, 0f);
+                letter.transform.localRotation = new Quaternion(0f, 0f, 1f, -1.08f);
+                letter.GetComponent<RectTransform>().sizeDelta = new Vector2(308f, 100f);
+                letterText.text = "説明0000000";
+                break;
+            case 8:
+                circle.transform.localPosition = new Vector3(-554.5f, 146.5f, 0f);
+                arrow.transform.localPosition = new Vector3(-453f, 117f, 0f);
+                arrow.transform.localRotation = new Quaternion(0f, 0f, 1f, 0.45f);
+
+                explanation.transform.localPosition = new Vector3(-375f, 259f, 0f);
+                explanation.transform.localRotation = new Quaternion(0f, 0f, 1f, 0.93f);
+                explanation.transform.localScale = new Vector3(3.5f, 1.2f, 1.2f);
+                letter.transform.localPosition = new Vector3(-372f, 261f, 0f);
+                letter.transform.localRotation = new Quaternion(0f, 0f, 1f, -1.08f);
+                letter.GetComponent<RectTransform>().sizeDelta = new Vector2(308f, 100f);
+                letterText.text = "説明0000000";
+                break;
+            case 9:
+                progress = 0;
+                break;
+
+        }
     }
 }
